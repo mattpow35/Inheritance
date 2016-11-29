0211e8d109aedbc3bfd7c98df46183afcfe8339f
mattpow35 <mpow2231@740s-pl-2.local>
2016-11-29 09:53:48 -0700

    practiced interfaces.

--- src/stuff/model/ThirdThing.java
+++ src/stuff/model/ThirdThing.java
@@ -1,6 +1,37 @@
 package stuff.model;
 
-public class ThirdThing
+public class ThirdThing extends SubThing implements UnDoAble
 {
-
+	public void removesThingsFromNumberedSlots(int [] stuff)
+	{
+		
+	}
+	
+	public void undoesThat()
+	{
+		
+	}
+	
+	public int undoesOtherThings()
+	{
+		return Integer.MIN_VALUE;
+	}
+	
+	public int doesOtherThings(int NumberOfTimes)
+	{
+		return Integer.MAX_VALUE;
+	}
+	
+	public int [] shovesThingsIntoNumberSlots()
+	{
+		int [] temp = new int[5];
+		
+		return temp;
+	}
+	
+	public void doesThat()
+	{
+		System.out.println("sub class version");
+		super.doesThat();
+	}
 }
